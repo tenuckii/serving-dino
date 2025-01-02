7091323d1d85ae34a4cb3951698f6643ecc4c78c
tenuckii <152208465+tenuckii@users.noreply.github.com>
2025-01-01 23:57:33 -0500

    comments

--- route.ts
+++ route.ts
@@ -1,4 +1,17 @@
+/**
+ * A class to represent all available routes
+ */
 export default class Route {
+    /** Singlethon instance of the class */
+    private static _instance: Route;
+
+    /** Hashmap containing all available routes */
+    private dict: Map<string, endpoint>;
+  
+    /**
+     * Handle all incoming request and execute the correct fonction depending on the request
+     * @param _req incomming http request.
+     */
     public handler(_req: Request): Response {
         if (this.dict.size === 0)
             return new Response("Not Found", { status: 404 });
@@ -11,6 +24,10 @@ export default class Route {
         return entry?.fn({ body: this.body(_req), headers: _req.headers, request: _req })
     }
 
+    /**
+     * Gets the available format for the body
+     * @param _req incomming http request.
+     */
     private async body(req: Request): Promise<JSON | string | undefined> {
         const contentType = req.headers.get("content-type");
 
@@ -25,21 +42,31 @@ export default class Route {
         }
 
     }
-    public path(str_path: string, method: Methods, fn: (...args: any) => Response) {
-        this.dict.set(str_path, { method, fn })
+
+    /**
+     * Update path hashmap with new routes
+     * @param strPath path
+     * @param method method used by the request.
+     * @param fn the function needed for the request.
+     */
+    public path(strPath: string, method: Methods, fn: (...args: any) => Response) {
+        this.dict.set(strPath, { method, fn })
     }
 
+    /**
+     * Initializing of the singlethon
+     */
     public static route():Route {
         return this._instance || (this._instance = new this);
     }
     private constructor() {
         this.dict = new Map();
     }
-
-    private static _instance: Route;
-    private dict: Map<string, endpoint>;
 }
 
+/**
+ * Enum for all available methods
+ */
 export enum Methods {
     GET = "GET",
     HEAD = "HEAD",
@@ -52,12 +79,18 @@ export enum Methods {
     PATCH = "PATCH"
 }
 
+/**
+ * Available parameter for the function
+ */
 export interface Options{
     body: Promise<JSON | string | undefined>,
     headers: Headers,
     request: Request
 }
 
+/**
+ * 
+ */
 interface endpoint {
     method: Methods,
     fn: (...args: any) => Response
