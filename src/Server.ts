9a998a038fd7dde865f1f07c0c347cc681540210
tenuckii <152208465+tenuckii@users.noreply.github.com>
2025-01-07 13:40:45 -0500

    tcpOptions

--- src/Server.ts
+++ src/Server.ts
@@ -9,11 +9,11 @@ export default class Server {
 	private running: boolean;
 
 	public static start(
-		options?: Deno.ServeTcpOptions,
+		options?: ServeTcpOptions,
 	): Server {
 		return this._instance || (this._instance = new this(options));
 	}
-	constructor(options?: Deno.ServeTcpOptions) {
+	constructor(options?: ServeTcpOptions) {
 		this._route = Route.route();
 		this.running = true;
 		if (options === undefined) {
@@ -102,3 +102,28 @@ export default class Server {
 		this._route.path(path, Methods.PATCH, func);
 	}
 }
+
+interface ServeTcpOptions extends Deno.ServeOptions<Deno.NetAddr> {
+    /** The transport to use. */
+    transport?: "tcp";
+
+    /** The port to listen on.
+     *
+     * Set to `0` to listen on any available port.
+     *
+     * @default {8000} */
+    port?: number;
+
+    /** A literal IP address or host name that can be resolved to an IP address.
+     *
+     * __Note about `0.0.0.0`__ While listening `0.0.0.0` works on all platforms,
+     * the browsers on Windows don't work with the address `0.0.0.0`.
+     * You should show the message like `server running on localhost:8080` instead of
+     * `server running on 0.0.0.0:8080` if your program supports Windows.
+     *
+     * @default {"0.0.0.0"} */
+    hostname?: string;
+
+    /** Sets `SO_REUSEPORT` on POSIX systems. */
+    reusePort?: boolean;
+  }
